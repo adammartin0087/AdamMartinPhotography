2890acc97970abf9b2a1d091c35ef3ab41ab1289
Adam Martin <adammartin0087@gmail.com>
2020-04-26 10:30:53 -0600

    Mapbox Updates

--- src/containers/explore/explore-detail.jsx
+++ src/containers/explore/explore-detail.jsx
@@ -34,26 +34,29 @@ class ExploreDetail extends Component {
     return (
       image && (
         <div className="explore-detail mt-1">
-          <div className="text-center">{image.imageUrl && <ShadowImage src={getImageUrl(image.imageUrl, 520, 300)} alt={image.name} />}</div>
+          <div>
+            <div className="text-center">{image.imageUrl && <ShadowImage src={getImageUrl(image.imageUrl, 520, 300)} alt={image.name} />}</div>
 
-          <div className="monumentDetails">
-            <h1 className="title">{image.name}</h1>
-            <div className="description">
-              <Icon icon="map-marker" style={{ width: 14 }} className="mr-2" /> {image.location}
+            <div className="monumentDetails">
+              <h1 className="title">{image.name}</h1>
+              <div className="description">
+                <Icon icon="map-marker" style={{ width: 14 }} className="mr-2" /> {image.location}
+              </div>
             </div>
           </div>
-
-          <div className="buttons">
-            <div className="navbutton" onClick={() => this.onCycleImage(image.code, -1)}>
-              <Icon icon="arrow-left" className="mr-2" />
-              Previous
-            </div>
-            <div className="navbutton" onClick={() => this.onCycleImage(image.code, 1)}>
-              Next
-              <Icon icon="arrow-right" className="ml-2" />
-            </div>
-            <div className="navbutton" style={{ flexGrow: 1 }} onClick={this.onGoBack}>
-              <small>EXIT</small>
+          <div>
+            <div className="buttons">
+              <div className="navbutton" onClick={() => this.onCycleImage(image.code, -1)}>
+                <Icon icon="arrow-left" className="mr-2" />
+                Previous
+              </div>
+              <div className="navbutton" onClick={() => this.onCycleImage(image.code, 1)}>
+                Next
+                <Icon icon="arrow-right" className="ml-2" />
+              </div>
+              <div className="navbutton" style={{ flexGrow: 1 }} onClick={this.onGoBack}>
+                <small>EXIT</small>
+              </div>
             </div>
           </div>
         </div>
