89c63d65b0b415cdbf1ebd6abd76eb742dffd4e4
Adam Martin <adammartin0087@gmail.com>
2020-04-27 09:20:24 -0600

    Remove CSS Module

--- src/components/image-map/map-popup.jsx
+++ src/components/image-map/map-popup.jsx
@@ -1,18 +1,17 @@
 import * as React from "react";
 import { Popup } from "react-mapbox-gl";
 import ShadowImage from "./../common/shadow-image";
-import styles from "./popup.module.css";
 
 const offset = [0, -15];
 
 const MapPopup = ({ image }) => (
   <div>
     <Popup coordinates={[image.longitude, image.latitude]} anchor="bottom" offset={offset}>
-      <div className={styles.container}>
-        <ShadowImage className={styles.image} src={image.imageUrl} alt={image.name} width="240" />
-        <div className={styles.footer}>
-          <h2 style={{ fontSize: 15 }}>{image.name}</h2>
-          <span className={styles.sub}>{image.location}</span>
+      <div className="text-center">
+        <ShadowImage src={image.imageUrl} alt={image.name} width="240" />
+        <div className="map-caption">
+          <h2>{image.name}</h2>
+          <span>{image.location}</span>
         </div>
       </div>
     </Popup>
